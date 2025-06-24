ecdc2f4509a75e434a44c8b3a601161f59874a1c
Ruchita Sengupta <senguptaruchita@gmail.com>
2025-06-24 13:08:46 +0530

    Update final_train.py

--- Task_A/final_train.py
+++ Task_A/final_train.py
@@ -5,23 +5,6 @@ import zipfile
 import os
 import torch
 
-def load_model(model: torch.nn.Module,
-               model_save_path: str,device):
-    """
-    Loads model weights from a .pth file into the given model.
-
-    Args:
-        model: PyTorch model architecture to load weights into
-        model_save_path: Path to the saved .pth weights file
-        device:The device model is on
-    """
-    # Load saved weights into model
-    weights = model.load_state_dict(torch.load(model_save_path,map_location=device))
-
-    # Print all layer names and shapes
-    for name, param in weights.items():
-        print(f"{name}: {param.shape}")
-
 
 def download_and_extract_dataset():
     # Dataset URL (replace with your actual Google Drive link)
@@ -82,4 +65,5 @@ evaluation_metrics.print_metrics(metrics)
 model_save_path=utils.save_model(model,
                target_dir="models",
                model_name="Task_A_gender_classification_weights.pth")
+
 
